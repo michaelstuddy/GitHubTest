aded49692a7057b8064540fe8c5ccdfd3df4bc51
Michael Ruddy <mrud2001@outlook.com>
2017-07-18 19:24:29 -0700

    added table view

--- GITHUBTEST/ViewController.swift
+++ GITHUBTEST/ViewController.swift
@@ -3,23 +3,46 @@
 //  GITHUBTEST
 //
 //  Created by Michael Ruddy on 7/17/17.
-//  Copyright © 2017 Michael Ruddy. All rights reserved..
+//  Copyright © 2017 Michael Ruddy. All rights reserved..99999
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+    @IBOutlet weak var tableview: UITableView!
+    var emojis = ["😃","😎","😉","🤤","😭","👍","🤷‍♀️","💁"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableview.dataSource = self
+        tableview.delegate = self
+        
+        
+        
+        
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
+
 
